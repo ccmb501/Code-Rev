e9616e301a1848db64509d576c9fefc7f29cb259
ccmb501 <55030572+ccmb501@users.noreply.github.com>
2019-09-07 21:47:14 -0500

    Add files via upload

--- CodeRev.py
+++ CodeRev.py
@@ -1,7 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep  7 12:46:19 2019
+BuildingDict = {
+        "mosher":{"spider man": "066",
+        "doctor strange": "123",
+        "peter quill": "234",
+        "iron man": "066",
+        "incredible hulk": "456",
+        "black widow": "090",
+        "hawk eye": "270"
+}
+}
 
-@author: Cade
-"""
 
+userNameInput = input("Please enter your name: ")
+userRoomInput = str(input("Please enter your room number: "))
+userBuildingInput = str(input("Please enter your hall building name: "))
+
+userNameInput = str.lower(userNameInput)
+userBuildingInput = str.lower(userBuildingInput)
+
+if userBuildingInput in BuildingDict:
+    if userNameInput in BuildingDict["mosher"]:
+        if userRoomInput == BuildingDict["mosher"][userNameInput]:
+            print("true")
+        else:
+            print("false")
